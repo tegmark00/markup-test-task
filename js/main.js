1f36794edecb36233c57286cd84e2a186202a003
Mark Bobkov <mark.bobkov.13@gmail.com>
2021-08-31 23:46:14 +0300

    Fixed slider

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
-onload = function () {
-    console.log('it works!')
-}
+$(function() {
+    // Owl Carousel
+    let owl = $(".owl-carousel");
+    owl.owlCarousel({
+        items: 1,
+        loop:true,
+        autoplay:true,
+        autoplayTimeout:3000,
+        autoplayHoverPause:true,
+        // dots:true,
+    });
+  });
